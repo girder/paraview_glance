595187984becdf6ee764f3fa8fbf7eb4afccc0f0
subdavis <brandonadavis@outlook.com>
2018-10-09 13:42:26 -0400

    First pass -- embed video

---
+++ src/views/VideoDialog.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+v-dialog(v-model="dialog", :width="width")
+  template(slot="activator")
+    slot(name="activator")
+  iframe(v-if="dialog"
+      :src="src",
+      :width="width",
+      :height="height",
+      frameborder="0",
+      webkitallowfullscreen,
+      mozallowfullscreen,
+      allowfullscreen)
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style lang="stylus">
+.v-overlay--active:before
+  opacity .82 !important
+</style>
